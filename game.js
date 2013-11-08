f55cb04aa764fb63ff9b98d0375299a03e0bafe6
Rob Finean <rfinean@iee.org>
2013-11-08 12:36:19 +0000

    app cache manifest and combining files to speed loading

--- game.js
+++ game.js
@@ -15,6 +15,7 @@ var gameboard = document.getElementById("gameboard"),
 var score = 0,
 	question = 0,
 	qn = {},
+	answered = [],	// Array of (phrases + language) that have already been asked
 	timer = 10,
 	timeout = null,
 	histogram = [];
@@ -82,6 +83,60 @@ this.checkAnswer = function() {
 this.playOn = function() {
 	maxquestions += 10;
 	this.nextQuestion();
+};
+
+function pickRandomProperty(obj) {
+	var keys = [];
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop)) {
+			keys.push(prop);
+		}
+	}
+	return keys[ keys.length * Math.random() << 0 ];
+}
+
+function shuffle(o) {
+	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+}
+
+function convertToSlug(text) {
+	return text.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'');
+}
+
+function pickAnswerAndChoices(questions, category, numberOfChoices) {
+	var keys = Object.keys(questions);	// List all the keys to the questions
+	var shuffledKeys = shuffle(keys);	// Shuffle them so they are in a random order
+
+	var answer = shuffledKeys[0];		// First one will be the answer
+	var language = shuffle(questions[answer])[0];	// Pick a random langauge for the answer
+	var choices = shuffle(shuffledKeys.slice(0,numberOfChoices));	// Pick & shuffle the first n choices, which includes the answer   
+	return {
+		language: language,
+		category: category,
+		answer: answer,
+		mp3: baseDirectory + '/' + language + '/' + convertToSlug(answer) + '.m4a',
+		choices: choices
+	};
+}
+
+function getChoices(data, numberOfChoices) {
+	var category = pickRandomProperty(data);
+	var questions = data[category];
+	return pickAnswerAndChoices(questions, category, numberOfChoices);
+}
+
+function newQuestion(data) {
+	var result;
+	// This is a horrible way for making sure the same audio clip doesn't appear twice
+	// but as long as the number of questions is far below the number of possible answers it will be ok.
+	do {
+		// Keep looping until we find an unseen answer
+		result = getChoices(data, 4);
+	}
+	while (answered.indexOf(result.answer + result.language) >= 0);
+	answered.push(result.answer + result.language);
+	return result;
 }
 
 function displayResult(message){
